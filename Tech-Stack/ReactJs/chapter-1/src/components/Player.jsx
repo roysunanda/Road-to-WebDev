63f32cf3066e0d20c769f5edbb737cfe9e7a5889
Sunanda Roy <roysunanda.update@gmail.com>
2026-06-09 19:36:16 +0530

    project updated

--- Tech-Stack/ReactJs/chapter-1/src/components/Player.jsx
+++ Tech-Stack/ReactJs/chapter-1/src/components/Player.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const playerNameVar = isEditing ? (
+    <input type='text' required />
+  ) : (
+    <span className='player-name'>{name}</span>
+  );
+
   return (
     <li>
       <span className='player'>
-        <span className='player-name'>{name}</span>
+        {playerNameVar}
         <span className='player-symbol'>{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 };
